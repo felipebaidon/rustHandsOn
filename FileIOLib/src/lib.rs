bef409dd874ef7404239303a2497d4ab2ba98f66
FelipeBaidon <felipe.baidon94@gmail.com>
2026-01-24 13:58:18 -0600

    copied code from previous setup

--- FileIOLib/src/lib.rs
+++ FileIOLib/src/lib.rs
@@ -1,5 +1,60 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::io::BufRead;
+use std:: fs:: File;
+use std:: io:: BufReader;
+
+pub fn create_file(filename: String) -> u8
+{
+    let result = File::create(filename);
+    println!("{:?}", result);
+    return 0
+
+}
+
+// pub fn overwrite_file() -> u8
+// {
+//     let result = File::create(filename);
+//     result
+
+// }
+
+pub fn open_file(filename: String) -> u8
+{
+    let file = File::open(filename);
+    let result = match file {
+        Ok(file) => 0,
+        Err(error) => 1,
+    };
+
+    result
+}
+
+pub fn read_file(filename: String) -> u8
+{
+    let file = File::open(filename);
+    let mut result = 1;
+    match file {
+        Ok(file) => 
+        {
+            let reader = BufReader::new(file);
+            for line in reader.lines() {
+                println!("{}", line.unwrap());
+            }
+            result = 0;
+        }
+        Err(error) => match error.kind()
+        {
+            std::io:: ErrorKind:: NotFound => {
+                println!("File not found! {}", error);
+            }
+            _=> {
+                println!("Error opening file: {}", error);
+                }
+
+        },
+
+    };
+    
+    result
 }
 
 #[cfg(test)]
@@ -7,8 +62,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_open_nonexistentfile()
+    {
+        let result = open_file("non_existent_fle.txt".to_string());
+        assert_eq!(result,1);
     }
+
+    #[test]
+    fn test_open_existentfile()
+    {
+        let result = open_file("src/test.txt".to_string());
+        assert_eq!(result,0);
+    }
+
+    #[test]
+    fn test_existent_file_read()
+    {
+        let result = read_file("src/test.txt".to_string());
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_nonexistent_file_read()
+    {
+        let result = read_file("non-existent-file.txt".to_string());
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_file_creation()
+    {
+        let result = create_file("newfile.txt".to_string());
+        assert_eq!(result, 0);
+    }
+
 }
