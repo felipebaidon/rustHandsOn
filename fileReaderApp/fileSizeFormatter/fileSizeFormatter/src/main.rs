3b1db9d5211ec8ab84db186d5170f98a4b9a72e4
FelipeBaidon <felipe.baidon94@gmail.com>
2026-01-24 13:49:30 -0600

    corrupted file

--- fileReaderApp/fileSizeFormatter/fileSizeFormatter/src/main.rs
+++ fileReaderApp/fileSizeFormatter/fileSizeFormatter/src/main.rs
@@ -9,18 +9,19 @@ enum FileSize {
 
 #[derive(Debug)]
 struct FileSizeStruct {
-    bytes: u64,
-    kilobytes: f64,
+    bytes : u64, 
+    kilobytes : f64,
     megabytes: f64,
     gigabytes: f64,
 }
 
-fn Unformat_size(size: u64, sizeSuffix: &str) -> u64 {
-    let storage = match sizeSuffix.to_lowercase().as_str() {
-        "kb" => size * 1000,
-        "mb" => size * 1_000_000,
-        "gb" => size * 1_000_000_000,
-        _ => size,
+fn Unformat_size(size: u64, sizeSuffix: &str) -> u64
+{
+        let storage = match sizeSuffix.to_lowercase().as_str() {
+        "kb"=> size*1000,
+        "mb"=> size*1_000_000,
+        "gb"=> size*1_000_000_000,
+        _=> size,
     };
 
     storage
@@ -36,26 +37,23 @@ fn format_size(filesize: &FileSize) -> String {
 }
 
 fn all_sizes(size: u64) -> FileSizeStruct {
-    let storage = FileSizeStruct {
-        bytes: size,
-        kilobytes: size as f64 / 1000.0,
-        megabytes: size as f64 / 1000000.0,
-        gigabytes: size as f64 / 1000000000.0,
-    };
-
+    let storage = FileSizeStruct{bytes: size, kilobytes: size as f64 /1000.0, megabytes: size as f64/1000000.0, gigabytes:size as f64/1000000000.0};
+    
     storage
 }
 
+
 fn main() {
+
     let args: Vec<String> = env::args().collect();
-    let number = args[1].parse::<u64>().unwrap();
-    
-    //ToDO: add exception handling
+    let number = args[1].parse::<u64>().unwrap(); 
+    //ToDO: add exception handling 
     let sufix = &args[2];
 
     let resultBytes = Unformat_size(number, sufix);
 
     let result_list = all_sizes(resultBytes);
 
-    println!("{:?}", result_list);
+    println!("{:?}",result_list);
+
 }
